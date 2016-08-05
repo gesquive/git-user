e9d3469481b1d77a7e26ade54d560b15ef03d348
Gus Esquivel <gesquive@gmail.com>
2016-08-05 17:48:30 -0500

    fix app version output

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import "fmt"
 import "path/filepath"
 import "github.com/gesquive/git-user/cmd"
 
-var version = "2.0.0"
+var version = "v2.0.1"
 var dirty = ""
 
 func main() {
-	displayVersion := fmt.Sprintf("%s v%s%s",
+	displayVersion := fmt.Sprintf("%s %s%s",
 		filepath.Base(os.Args[0]),
 		version,
 		dirty)
