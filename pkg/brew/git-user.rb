24212a99f58a7ec30fd5b7cce749d1e1f2806d77
Gus Esquivel <gesquive@gmail.com>
2017-06-07 08:59:55 -0500

    update brew formula

--- pkg/brew/git-user.rb
+++ pkg/brew/git-user.rb
@@ -1,16 +1,16 @@
 class GitUser < Formula
-  version '2.0.4'
+  version '2.0.5'
   desc "Git plugin that allows you to save multiple user profiles and set them as project defaults"
   homepage "https://github.com/gesquive/git-user"
   url "https://github.com/gesquive/git-user/releases/download/v#{version}/git-user-v#{version}-osx-x64.tar.gz"
-  sha256 "f14846abbcd9dedabf1dcd33c9a8e6d7267d43edcd87e30f81b74d1514349ef8"
+  sha256 "afe38cd5f6e7d93f54f7a85b8ba0e4fec4d68d9a3d1b74c53cfda3b83de7bfd5"
 
   conflicts_with "git-user"
 
   def install
     bin.install "git-user"
     man.mkpath
-    man1.install "man/git-user.1", "man/git-user_add.1", "man/git-user_del.1", "man/git-user_edit.1", "man/git-user_list.1", "man/git-user_rm.1", "man/git-user_set.1"
+    man1.install "man/git-user.1", "man/git-user-add.1", "man/git-user-del.1", "man/git-user-edit.1", "man/git-user-list.1", "man/git-user-rm.1", "man/git-user-set.1"
   end
 
   test do
